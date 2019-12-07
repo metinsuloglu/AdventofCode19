a6472d53911b76894d8f2ac52a678208d1e432fe
Metin Suloglu <metinsuloglu97@gmail.com>
2019-12-07 11:17:50 +0000

    Day 7 improvements

--- day7.py
+++ day7.py
@@ -14,12 +14,10 @@ print('~*~*~ Day 7 ~*~*~')
 memory = IntCode.read_memory('day7.csv')
     
 max_signal = -float('inf')
-for (A,B,C,D,E) in permutations(range(0, 5)):
-    _, outputs, _ = IntCode(memory.copy()).run(inputs=[A, 0], capture_output=True)
-    _, outputs, _ = IntCode(memory.copy()).run(inputs=[B] + outputs, capture_output=True)
-    _, outputs, _ = IntCode(memory.copy()).run(inputs=[C] + outputs, capture_output=True)
-    _, outputs, _ = IntCode(memory.copy()).run(inputs=[D] + outputs, capture_output=True)
-    _, outputs, _ = IntCode(memory.copy()).run(inputs=[E] + outputs, capture_output=True)
+for p in permutations(range(0, 5)):
+    outputs = [0]
+    for amp in range(5):
+        _, outputs, _ = IntCode(memory.copy()).run(inputs=[p[amp]] + outputs, capture_output=True)
     
     max_signal = max(outputs[0], max_signal)
 
@@ -27,19 +25,14 @@ print('(Part One)')
 print(max_signal)
 
 max_signal = -float('inf')
-for (A,B,C,D,E) in permutations(range(5, 10)):
+for p in permutations(range(5, 10)):
     amps = [IntCode(memory.copy()) for _ in range(5)]
-    _, outputs, hc = amps[0].run(inputs=[A, 0], capture_output=True)
-    _, outputs, hc = amps[1].run(inputs=[B] + outputs, capture_output=True)
-    _, outputs, hc = amps[2].run(inputs=[C] + outputs, capture_output=True)
-    _, outputs, hc = amps[3].run(inputs=[D] + outputs, capture_output=True)
-    _, outputs, hc = amps[4].run(inputs=[E] + outputs, capture_output=True)
+    outputs = [0]
+    for amp in range(5):
+        _, outputs, hc = amps[amp].run(inputs=[p[amp]] + outputs, capture_output=True)
     while hc != 99:
-        _, outputs, hc = amps[0].run(inputs=outputs, capture_output=True)
-        _, outputs, hc = amps[1].run(inputs=outputs, capture_output=True)
-        _, outputs, hc = amps[2].run(inputs=outputs, capture_output=True)
-        _, outputs, hc = amps[3].run(inputs=outputs, capture_output=True)
-        _, outputs, hc = amps[4].run(inputs=outputs, capture_output=True)
+        for amp in range(5):
+            _, outputs, hc = amps[amp].run(inputs=outputs, capture_output=True)
     
     max_signal = max(outputs[0], max_signal)
 
