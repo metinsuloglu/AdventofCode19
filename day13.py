b0db7d5a89c4850fb19a7f3433d7010f1ba8607a
Metin Suloglu <metinsuloglu97@gmail.com>
2019-12-13 12:21:39 +0000

    Update day13.py

--- day13.py
+++ day13.py
@@ -15,9 +15,9 @@ def play(arcade):
     while len(outputs):
         for o in [outputs[i:i+3] for i in range(0, len(outputs), 3)]:
             if o[0] == -1 and o[1] == 0: score = o[2]
-            elif o[2] == 3: paddle_loc = (o[0], o[1])
-            elif o[2] == 4: ball_loc = (o[0], o[1])
-        _, outputs, _ = arcade.run(inputs=[np.sign(ball_loc[0] - paddle_loc[0])], capture_output=True)
+            elif o[2] == 3: paddle_loc = o[0]
+            elif o[2] == 4: ball_loc = o[0]
+        _, outputs, _ = arcade.run(inputs=[np.sign(ball_loc - paddle_loc)], capture_output=True)
     return score
 
 memory = IntCode.read_memory('day13.csv')
